93b289f377b6bf11b28bf9b88f15396252ebcd07
David Buckley <dbuckl02>
2017-01-24 19:11:06 -0500

    Fixing command-line version for testing on socs server

--- Client/client_cmd.c
+++ Client/client_cmd.c
@@ -15,6 +15,8 @@ pthread_mutex_t connection_lock;
 int sockfd_conn = -1;
 int sockfd_msg = -1;
 
+int sockfd_msg_interrupt[2];
+
 struct sockaddr_in server_conn;
 struct sockaddr_in server_msg;
 
@@ -34,9 +36,26 @@ int error_exit(char* message) {
     exit(1);
 }
 
+void sever_connection() {
+    printf("Disconnecting from server...\n");
+
+    write(sockfd_msg_interrupt[1], "close", 5); //Will cause message listener to kill itself
+    write(sockfd_conn, "DISCONNECT", 10);
+    //close(sockfd_conn);
+
+    close(sockfd_conn);
+    close(sockfd_msg);
+
+    connected = 0;
+
+    sockfd_conn = -1;
+    sockfd_msg = -1;
+}
+
 int main(int argc, char *argv[])
 {
     char buffer[BUFFERSIZE];
+
     if (pthread_mutex_init(&socket_lock, NULL) != 0) {
         printf("mutex init failed");
         return 1;
@@ -47,13 +66,26 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    pipe(sockfd_msg_interrupt);
+
     connect_to_server();
-    printf("Enter message:\n");
-    scanf("%s", buffer);
+    while (strcmp(buffer, "QUIT") != 0) {
+        memset(&buffer, 0, sizeof(buffer));
+        printf("Enter message: ");
+        fgets(buffer, BUFFERSIZE, stdin);
 
-    send_message(buffer, BUFFERSIZE);
+        if (strcmp(buffer, "!QUIT") == 0) {
+            break;
+        } else if (strcmp(buffer, "!DISS") == 0) {
+            sever_connection();
+        } else if (strcmp(buffer, "!CONN") == 0) {
+            connect_to_server();
+        } else {
+            send_message(buffer, BUFFERSIZE);
+        }
+    }
 
-    pthread_join(message_thread, NULL);
+    pthread_cancel(message_thread);
     pthread_mutex_destroy(&socket_lock);
     pthread_mutex_destroy(&connection_lock);    
  
@@ -80,44 +112,53 @@ int handshake(int sockfd) {
     if(n < 0) {
        error_exit("Read Error");
     }
-    printf("\"%s\" - Recieved\n", recvBuff);
+    //printf("\"%s\" - Recieved\n", recvBuff);
 
     if (strncmp(recvBuff, "CONFIRM", 7) == 0) {
-        printf("Confirming\n");
+        //printf("Confirming\n");
         write(sockfd, "CONNECT", 7);
         return 1;
     } else {
-        printf("Not confirmed\n");
+        //printf("Not confirmed\n");
         return 0;
     }
 }
 
 void* listen_for_new_message() {
     fd_set rfds; //FD to listen for new activity on
-    struct timeval tv;
     char buffer[BUFFERSIZE];
-    int retval = 0, newsockfd = 0, n = 0;
 
-    tv.tv_sec = 7;  /* 7 Secs read Timeout */
-    tv.tv_usec = 0;
+    int retval = 0, n = 0, maxfd = -1;
 
-    FD_ZERO(&rfds);
-    FD_SET(sockfd_msg, &rfds);
     while (1) {
+        FD_ZERO(&rfds);
+        FD_SET(sockfd_msg, &rfds);
+        FD_SET(sockfd_msg_interrupt[0], &rfds);
+
+        maxfd = (sockfd_msg > sockfd_msg_interrupt[0]) ? sockfd_msg : sockfd_msg_interrupt[0];
         memset(&buffer, 0, sizeof(buffer));
-        retval = select(sockfd_msg+1, &rfds, NULL, NULL, NULL); //Wait for activity on the listening port
+        retval = select(maxfd+1, &rfds, NULL, NULL, NULL); //Wait for activity on the listening port
 
         if (retval == -1) {
             error_exit("ERROR on select");
         }
 
-        printf("Connection is available - Message\n");
-        n = read(sockfd_msg, buffer, BUFFERSIZE);
-        if (n < 0) {
-            error_exit("ERROR on read");
+        if (FD_ISSET(sockfd_msg_interrupt[0], &rfds)) {
+            printf("No longer listening for messages\n");
+            //close(sockfd_msg);
+            return NULL;
         }
 
-        printf("Recieved message: \"%s\"\n", buffer);
+        //printf("Connection is available - Message\n");
+        n = read(sockfd_msg, buffer, BUFFERSIZE);
+        //printf("read %d chars: %s\n", n, buffer);
+
+        if (n <= 0) {
+            printf("Network error occurued, disconnecting from server...\n");
+            sever_connection();
+            return NULL;
+        }
+        printf("Message recieved: \"%s\"", buffer);
     }
 
     return NULL;
@@ -126,74 +167,76 @@ void* listen_for_new_message() {
 void connect_to_server()
 {
     pthread_mutex_lock(&connection_lock);
-        if (!connected) {
-            int shake = 0, err = 0;
-            char server[] = "cis4110chatserver.davidbuckleyprogrammer.me";
-            char* restrict server_ip;
-            char recvBuff[BUFFERSIZE];
+    if (!connected) {
+        int shake = 0, err = 0;
+        char server[] = "cis4110chatserver.davidbuckleyprogrammer.me";
+        char* restrict server_ip;
+        char recvBuff[BUFFERSIZE];
 
-            printf("Connecting to server...\n");
+        printf("Connecting to server...\n");
 
-            server_ip = ipv4_dns_resolve(server);
+        server_ip = ipv4_dns_resolve(server);
+        
+        memset(recvBuff, 0,sizeof(recvBuff));
+
+        pthread_mutex_lock(&socket_lock);
+            if((sockfd_conn = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                error_exit("Error : Could not create socket");
+            }
+            if((sockfd_msg = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                error_exit("Error : Could not create socket");
+            } 
             
-            memset(recvBuff, 0,sizeof(recvBuff));
+            printf("Sockets created...\n");
 
-            pthread_mutex_lock(&socket_lock);
-                if((sockfd_conn = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-                    error_exit("Error : Could not create socket");
-                }
-                if((sockfd_msg = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-                    error_exit("Error : Could not create socket");
-                } 
-                
-                printf("Sockets created...\n");
+            memset(&server_conn, 0, sizeof(server_conn));
+            memset(&server_msg, 0, sizeof(server_msg));
 
-                memset(&server_conn, 0, sizeof(server_conn));
-                memset(&server_msg, 0, sizeof(server_msg));
+            server_conn.sin_family = AF_INET;
+            server_conn.sin_port = htons(CONNECTION_PORT);
 
-                server_conn.sin_family = AF_INET;
-                server_conn.sin_port = htons(CONNECTION_PORT);
+            server_msg.sin_family = AF_INET;
+            server_msg.sin_port = htons(MESSAGE_PORT);
 
-                server_msg.sin_family = AF_INET;
-                server_msg.sin_port = htons(MESSAGE_PORT);
-
-                if(inet_pton(AF_INET, server_ip, &server_conn.sin_addr) <= 0) {
-                    error_exit("inet_pton error occured on connection port");
-                }
-                if(inet_pton(AF_INET, server_ip, &server_msg.sin_addr) <= 0) {
-                    error_exit("inet_pton error occured on message port");
-                }
-                printf("Connected to ip %s...\n", server_ip);
-                free(server_ip);
+            if(inet_pton(AF_INET, server_ip, &server_conn.sin_addr) <= 0) {
+                error_exit("inet_pton error occured on connection port");
+            }
+            if(inet_pton(AF_INET, server_ip, &server_msg.sin_addr) <= 0) {
+                error_exit("inet_pton error occured on message port");
+            }
+            printf("Connected to ip %s...\n", server_ip);
+            free(server_ip);
 
 
-                if(connect(sockfd_conn, (struct sockaddr *)&server_conn, sizeof(server_conn)) < 0) {
-                   error_exit("Error : Connect Failed on connection port");
-                }
-
-                printf("Doing handshake - CONNECTION_PORT\n");
-                shake = handshake(sockfd_conn);
-                printf("Done handshake, connection to message port\n");
-
-                if(connect(sockfd_msg, (struct sockaddr *)&server_msg, sizeof(server_msg)) < 0) {
-                   error_exit("Error : Connect Failed on message port");
-                }
-
-                printf("Msg Socket FD: %d\nConn Socket FD: %d\n", sockfd_msg, sockfd_conn);
-            pthread_mutex_unlock(&socket_lock);
-
-            if (!shake) {
-                error_exit("Unable to establish link with server");
+            if(connect(sockfd_conn, (struct sockaddr *)&server_conn, sizeof(server_conn)) < 0) {
+               error_exit("Error : Connect Failed on connection port");
             }
 
-            connected = 1;
+            printf("Doing handshake - CONNECTION_PORT\n");
+            shake = handshake(sockfd_conn);
+            printf("Done handshake, connection to message port\n");
 
-            err = pthread_create(&(message_thread), NULL, &listen_for_new_message, NULL);
-            if (err != 0) {
-                error_exit("Can't create thread");
+            if(connect(sockfd_msg, (struct sockaddr *)&server_msg, sizeof(server_msg)) < 0) {
+               error_exit("Error : Connect Failed on message port");
             }
-            printf("Connected to server, awaiting messages...\n");
-            
+
+            printf("Msg Socket FD: %d\nConn Socket FD: %d\n", sockfd_msg, sockfd_conn);
+        pthread_mutex_unlock(&socket_lock);
+
+        if (!shake) {
+            error_exit("Unable to establish link with server");
         }
+
+        connected = 1;
+
+        err = pthread_create(&message_thread, NULL, &listen_for_new_message, NULL);
+        if (err != 0) {
+            error_exit("Can't create thread");
+        }
+        printf("Connected to server, awaiting messages...\n");
+    } else {
+        //Already connected
+    }
     pthread_mutex_unlock(&connection_lock);
+
 }
